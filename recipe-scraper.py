126d497830c91e221a21d7f69daa3d4723271e48
Adi <kaden.adiyat@gmail.com>
2025-10-18 15:51:31 +0400

    Extracted recipe title + extracted ingredients list

--- recipe-scraper.py
+++ recipe-scraper.py
@@ -8,3 +8,14 @@ print(f"Response status code: {response.status_code}")
 
 soup = BeautifulSoup(response.text, 'html.parser')
 print(soup.prettify()[:200])
+
+title_element = soup.find('h1')
+title = title_element.get_text(strip=True) if title_element else 'No title is found!'
+print(f"Recipe Title: {title}")
+
+ingredients_list = soup.find('section', id='ingredients').find('ul')
+ingredients = [li.get_text(strip=True) for li in ingredients_list.find_all('li')] if ingredients_list else []
+print("Ingredients:")
+for ingredient in ingredients:
+    print(f"- {ingredient}")
+    
